e66adea176f8d77f4ee106009bd6b6cc2e63770a
Aluis Marte <aluismarte@gmail.com>
2022-07-10 12:33:54 -0400

    Completando test de delete y update.

---
+++ src/test/java/edu/programacion/avanzada/aluismarte/project/command/handler/DeleteAddressCommandHandlerTest.java
@@ -0,0 +1,39 @@
+package edu.programacion.avanzada.aluismarte.project.command.handler;
+
+import edu.programacion.avanzada.aluismarte.project.command.DeleteAddressCommand;
+import edu.programacion.avanzada.aluismarte.project.model.response.adress.DeleteAddressResponse;
+import edu.programacion.avanzada.aluismarte.project.repositories.AddressRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+/**
+ * @author aluis on 7/10/2022.
+ */
+@SpringBootTest
+class DeleteAddressCommandHandlerTest {
+
+    @Mock
+    private AddressRepository addressRepository;
+
+    @BeforeEach
+    void setup() {
+    }
+
+    @Test
+    void createAddressTest() {
+        DeleteAddressCommand deleteAddressCommand = DeleteAddressCommand.builder()
+                .id(1L)
+                .build();
+        Mockito.doNothing().when(addressRepository).deleteById(ArgumentMatchers.eq(1L));
+        DeleteAddressCommandHandler deleteAddressCommandHandler = new DeleteAddressCommandHandler(addressRepository);
+        DeleteAddressResponse deleteAddressResponse = deleteAddressCommandHandler.handle(deleteAddressCommand);
+        assertNotNull(deleteAddressResponse);
+    }
+
+}
---
+++ src/test/java/edu/programacion/avanzada/aluismarte/project/command/handler/UpdateAddressCommandHandlerTest.java
@@ -0,0 +1,53 @@
+package edu.programacion.avanzada.aluismarte.project.command.handler;
+
+import edu.programacion.avanzada.aluismarte.project.command.UpdateAddressCommand;
+import edu.programacion.avanzada.aluismarte.project.domain.Address;
+import edu.programacion.avanzada.aluismarte.project.model.dto.AddressDTO;
+import edu.programacion.avanzada.aluismarte.project.model.response.adress.UpdateAddressResponse;
+import edu.programacion.avanzada.aluismarte.project.repositories.AddressRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+/**
+ * @author aluis on 7/10/2022.
+ */
+@SpringBootTest
+class UpdateAddressCommandHandlerTest {
+
+    @Mock
+    private AddressRepository addressRepository;
+
+    @BeforeEach
+    void setup() {
+    }
+
+    @Test
+    void createAddressTest() {
+        UpdateAddressCommand createAddressCommand = UpdateAddressCommand.builder()
+                .id(1L)
+                .description("OMG")
+                .build();
+        Address addressMock = Mockito.spy(Address.class);
+        addressMock.setId(1L);
+        addressMock.setName("Hello");
+        addressMock.setDescription("");
+        Mockito.when(addressRepository.findById(ArgumentMatchers.eq(1L))).thenReturn(Optional.of(addressMock));
+        UpdateAddressCommandHandler updateAddressCommandHandler = new UpdateAddressCommandHandler(addressRepository);
+        UpdateAddressResponse updateAddressResponse = updateAddressCommandHandler.handle(createAddressCommand);
+        assertNotNull(updateAddressResponse);
+        AddressDTO address = updateAddressResponse.getAddress();
+        assertEquals(addressMock.getId(), address.getId());
+        assertEquals(addressMock.getName(), address.getName());
+        assertEquals(createAddressCommand.getDescription(), address.getDescription());
+    }
+
+}
