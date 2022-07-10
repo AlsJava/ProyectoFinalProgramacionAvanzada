a542158c48c9b332d697d7c1b1189f4381289d64
Aluis Marte <aluismarte@gmail.com>
2022-07-10 09:01:40 -0400

    Creando repositorios

---
+++ src/main/java/edu/programacion/avanzada/aluismarte/project/repositories/CheckoutRepository.java
@@ -0,0 +1,14 @@
+package edu.programacion.avanzada.aluismarte.project.repositories;
+
+import edu.programacion.avanzada.aluismarte.project.domain.Checkout;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+/**
+ * @author aluis on 7/10/2022.
+ */
+@Repository
+public interface CheckoutRepository extends JpaRepository<UUID, Checkout> {
+}
---
+++ src/main/java/edu/programacion/avanzada/aluismarte/project/repositories/OrderRepository.java
@@ -0,0 +1,14 @@
+package edu.programacion.avanzada.aluismarte.project.repositories;
+
+import edu.programacion.avanzada.aluismarte.project.domain.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+/**
+ * @author aluis on 7/10/2022.
+ */
+@Repository
+public interface OrderRepository extends JpaRepository<UUID, Order> {
+}
